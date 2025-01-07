561847aa83c446f29fee83eb7019452db202dd7d
HongTak <qoqo110966@gmail.com>
2025-01-08 03:54:23 +0900

    ch7 css 개요 02까지
    ch7 css 개요 02까지
    - 기본 문법, 주석,
    - 선언 방식

--- test/js/main.js
+++ test/js/main.js
@@ -1,1 +1,8 @@
 console.log('Hello World!asdas');
+
+const els = document.querySelectorAll('div');
+els.forEach((el) => {
+  el.addEventListener('click', () => {
+    console.log('click');
+  });
+});
